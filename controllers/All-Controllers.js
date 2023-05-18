016c9f612578d24d515775b60f8765b42c2b11d1
anu singh <annusingh58@gmail.com>
2023-05-18 14:09:58 +0530

    updates

---
+++ controllers/All-Controllers.js
@@ -0,0 +1,20 @@
+export const kunal=(req,res)=>{
+    res.send("sending message From kunal function");
+
+}
+export const Anu=(req,res)=>{
+    res.send("sending message From Anu function");
+
+}
+export const Snhel=(req,res)=>{
+    res.send("sending message From Snhel function");
+
+}
+export const Abhi=(req,res)=>{
+    res.send("sending message From Abhi function");
+
+}
+export const Poonam=(req,res)=>{
+    res.send("sending message From Poonam function");
+
+}
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+import express from "express";
+import{kunal,Anu,Snhel,Poonam,Abhi} from './controllers/All-Controllers.js';
 
-  
-
-import express from"express";
 const app = express();
-app.listen(3000) //port
-app.get('/hello' , function(req, res ){res.send("hello india")})
+// app.get('/hello' , function(req, res ){res.send("hello india")}) 
+
+// app.get('/anu',function(){console.log("inside awdix function..")}); 
+//pass two parameter first parameter is path and second is function
+
+// app.get('/anu',function(req,res){res.send("anu function..")});
+// app.get('/snhel',function(req,res){res.send("snhel function..")});
+// app.get('/poonam',function(req,res){res.send("poonam function..")});
+// app.get('/abhi',function(req,res){res.send("abhi function..")});
+// app.get('/kunal',function(req,res){res.send("kunal function..")});
+
+app.get('/kunal',kunal);
+app.get('/anu', Anu);
+app.get('/poonam', Poonam);
+app.get('/abhi' ,Abhi);
+app.get('/snhel' , Snhel)
+
+
+// app.post();
+// app.patch();
+// app.put();
+// app.delete();
+
+
+app.listen(8000) //port  use for run
 
