8e673e47fee1451f0215b2653ecc2896a6f703f5
anu singh <annusingh58@gmail.com>
2023-05-18 11:33:26 +0530

    updates

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("hello")
-console.log("hello aasff")
+// console.log("hello")
+// console.log("hello aasff")
+// console.log("hiiii annu")
+  
+
+import express from"express";
+const app = express();
+app.listen(3000); //port
+
+app.get('/hello' , function(req, res ){res.send("hello india")})
