0968bf0bc4b70549dad9e46e07f7a27408719a70
anu singh <annusingh58@gmail.com>
2023-05-23 11:07:35 +0530

    updates

---
+++ test.js
@@ -0,0 +1,25 @@
+cocnsole.log(sname);
+let iname="let";
+let Iname="Let"
+console.log(lname);
+const awdizkey="bvvb";
+cname="cname";
+console.log(cname);
+
+// let reassign value
+// const cant reassign
+
+// var   reassign
+
+// closure
+function outerFunction(){
+    var mySurname="Awdiz";
+    console.log(myName)
+
+    function innerFunction(){
+        // var mySurname="Institue";
+        // console.log(mySurname)
+        console.log(myName)
+    }
+}
+return innerFunction();
