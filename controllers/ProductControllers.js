468a0d726edb62946d2e7b6c4ca2efbbdfd8dddd
anu singh <annusingh58@gmail.com>
2023-05-24 16:05:41 +0530

    updates

--- controllers/ProductControllers.js
+++ controllers/ProductControllers.js
@@ -1,9 +1,22 @@
 export const addProduct =(req,res)=>{
     try{
-        console.log(req.body)
-         // const{Name,Price, Image} =req.body;
-    // console.log(req.body,"req.body")
-    res.send(`hii add products`)
+        // console.log(req.body)
+
+         const{Name,Price, Image} =req.body;
+    // console.log(Name, Price ,Image, "req.body")
+    if(!Name) return res.send("Name is required")
+    if(!Price) return res.send("Price is required")
+       // condition is complusory
+       //store product data into database
+       //  const product =schema ,mongodb connection,mongodb predefined function
+        // user ko ok true respond
+
+
+
+    // res.send({Name ,Price,Image})
+    // return res.send({Name, Price, Image})
+    return res.send(product)  // object structureing
+
     } 
     catch(error){
         console.log(error);
--- test.js
+++ test.js
@@ -1,25 +1,71 @@
-cocnsole.log(sname);
-let iname="let";
-let Iname="Let"
-console.log(lname);
-const awdizkey="bvvb";
-cname="cname";
-console.log(cname);
+// var name="anu"
+// console.log(name,"Name here")
 
-// let reassign value
-// const cant reassign
+// anu Name here answer
 
-// var   reassign
 
-// closure
+// console.log(name,"Name here")
+// var name="anu"
+// undefined Name here answer
+
+
+// var
+// let
+// const
+//hoisting
+//closure
+
+//  var  sname; //declaration
+//  sname="anu"; //asign
+// console.log(sname);
+
+// var  sname; //declaration
+// sname="anu"; //asign
+// console.log(sname);
+
+// let lname;
+// lname="Let"
+// console.log(lname);
+
+// const cname=""
+// cname="cname";
+// console.log(cname);
+
+
+//let -reassign the value,hosting not applicable,redeclaration is not poosible
+//const - cant reassign the value,hosting not applicable,redeclaration is not poosible
+//var-reassign the value,hoisting applicable,redeclaration is poossible
+
+
+
+
+
+
+
+// cocnsole.log(sname);
+// let iname="let";
+// let Iname="Let"
+// console.log(lname);
+// const awdizkey="bvvb";
+// cname="cname";
+// console.log(cname);
+
+// // let reassign value
+// // const cant reassign
+
+// // var   reassign
+
+ // closure innerfunction can access uter function,its remember  scope of parents too
 function outerFunction(){
-    var mySurname="Awdiz";
-    console.log(myName)
+    var myName ="Awdiz";
+    // console.log(myName)
 
     function innerFunction(){
         // var mySurname="Institue";
-        // console.log(mySurname)
         console.log(myName)
+    
     }
+    return innerFunction;
 }
-return innerFunction();
+var myFunc = outerFunction; //{function:innerfunction} //var is also return with function
+console.log(myFunc());
