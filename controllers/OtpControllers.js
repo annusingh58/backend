e6429580b4fd14fce333613331d147ab35c83cae
anu singh <annusingh58@gmail.com>
2023-06-02 23:06:43 +0530

    updates

--- controllers/OtpControllers.js
+++ controllers/OtpControllers.js
@@ -1,9 +1,11 @@
 import { v4 as uuidv4} from 'uuid';
 import Users from '../modals/Users.js';
 
+
+// .................otp otpRegisteration......................
+
 export const otpRegisteration =async (req,res) => {
-   
-   
+     
  try{
     const{ number,email } =req.body;
     if(!number) return res.send("Number is required");
@@ -39,6 +41,8 @@ export const otpRegisteration =async (req,res) => {
     }
 }
 
+// .................otpCheckForRegister...................
+
 export const otpCheckForRegister = async(req,res) =>{
 try{
    const{number ,email,otp} = req.body;
@@ -66,4 +70,123 @@ try{
 }
 
 
+}
+
+
+
+// .................otpNumberRegistration...............
+
+
+export const otpNumberRegistration = async (req, res) =>{
+    try{
+        const {number} = req.body;
+        if(!number) return res.send("number not found.")
+        
+        var codeforNumber = uuidv4();
+        
+
+        const isNummberPresent = await Users.find({number}).exec();
+        if(isNummberPresent.length) return res.send("Number already used")
+        
+
+        const user = new Users({
+            number : number,
+            otpForNumber : codeforNumber,
+            isNumberVerified : false
+
+            
+        })
+        await user.save();
+        res.send("Check your mobile number  for otp.")
+
+    }catch(error){
+        return res.send(error);
+    }
+}
+
+
+
+// .............otpEmailRegistration..................................
+
+
+export const otpEmailRegistration = async (req, res) =>{
+    try{
+        const { number,email} = req.body;
+        if(!number) return res.send("number not found.")
+        if(!email) return res.send("email is required")
+        
+        var codeforEmail = uuidv4();
+
+        const isNummberPresent = await Users.find({number}).exec();
+        
+        if(isNummberPresent[0].number==number){
+            const user = await  Users.insert({number},{ email: email, otpForEmail : codeforEmail} )
+            console.log(user);
+            await user.save();
+            res.send("Check your mobile email for otp.")
+        }else{
+            return res.send("number is wrong")
+        } 
+
+
+       
+
+    }catch(error){
+        return res.send(error);
+    }
+}
+
+
+
+export const otpCkeckForNumber = async (req, res) => {
+    try{
+        const {number,otp} = req.body;
+        if(!number) return res.send("number is required");
+        if(!otp) return res.send("otp is required");
+
+        const user = await Users.find({number}).exec();
+        if(!user.length) return res.send("user not found");
+
+        if(user[0].otpForNumber== otp){
+
+            const user = await Users.findOneAndUpdate({ number }, { isNumberVerified: true }).exec();
+            await user.save();
+            // res.send("Check your mobile number  for OTP.")
+         return res.send("Nummber is verified");
+
+        }
+        return res.send("otp wrong plz try again");
+
+        // return res.send(user[0]);
+
+
+    }catch(error){
+        return res.send(error);
+    }
+}
+
+export const otpCkeckForEmail = async (req, res) => {
+    try{
+        const {email,otp} = req.body;
+        if(!email) return res.send("email is required");
+        if(!otp) return res.send("otp is required");
+
+        const user = await Users.find({email}).exec();
+        if(!user.length) return res.send("user not found");
+
+        if(user[0].otpForEmail== otp){
+            const user = await Users.findOneAndUpdate({ email }, { isEmailVerified: true }).exec();
+            await user.save();
+            // res.send("Check your mobile number  for otp.")
+         return res.send("Emaail is verified");
+
+        }
+        return res.send("otp wrong plz try again");
+
+        // return res.send(user[0]);
+
+
+    }catch(error){
+        return res.send(error);
+    }
 }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -9,6 +9,11 @@ import { addProduct,getAllProducts} from "../controllers/ProductControllers.js";
 import { checkEmail,checkName } from "../middleware/authMiddleware.js";
 import { otpRegisteration } from "../controllers/OtpControllers.js";
 import { otpCheckForRegister } from "../controllers/OtpControllers.js";
+
+// import { otpEmailRegistration ,otpNumberRegistration } from "../controllers/OtpControllers.js";
+
+import { otpCkeckForEmail, otpCkeckForNumber, otpEmailRegistration, otpNumberRegistration} from "../controllers/OtpControllers.js";
+
 var router = express.Router();
 
 // router.get('/login',login);
@@ -27,6 +32,9 @@ router.post('/getUserByEmail',getUserByEmail);
 
 router.post('/otp-register', otpRegisteration);
 router.post('/otp-check-register', otpCheckForRegister)
+
+router.post('/otpCkeckForNumber', otpCkeckForNumber);
+router.post('/otpCkeckForEmail', otpCkeckForEmail);
 
 
 
